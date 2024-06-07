96c1fd14e4b52df0492e828ff3628ec6e0249714
PrisacariAndrei <andrei.prisacari@corlab.tech>
2024-06-07 20:18:43 +0300

    first

---
+++ Blogapp/Controllers/PostsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogApp.Models;
+
+namespace BlogApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        private static List<Post> posts = new List<Post>();
+
+        public ActionResult Index()
+        {
+            return View(posts);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.Id = posts.Count + 1;
+                post.CreatedAt = DateTime.Now;
+
+                posts.Add(post);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(post);
+        }
+    }
+}
---
+++ Blogapp/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
